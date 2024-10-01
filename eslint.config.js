7d65c1ec15d53feaef22dbc1f8a5ca86079717f8
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-10-01 07:53:40 -0300

    chore: add eslint-plugin-react-compiler for enhanced linting support

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import tailwindcss from "eslint-plugin-tailwindcss";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -33,9 +34,11 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
       tailwindcss,
       "simple-import-sort": simpleImportSort,
+      "react-compiler": reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "react-compiler/react-compiler": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
