7b371c57c6e8000fc7341821a85e0eacae0636a9
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-12-17 18:44:59 -0300

    feat: enable react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgr from "vite-plugin-svgr";
 
+const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(), TanStackRouterVite(), react(), svgr(), basicSsl()],
+  plugins: [
+    nodePolyfills(),
+    TanStackRouterVite(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    svgr(),
+    basicSsl(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
